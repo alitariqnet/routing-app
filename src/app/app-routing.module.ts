7cec20270950e0526fc22839fc3a2fc9265c968e
alitariqnet <alibabapk@aol.com>
2024-03-10 22:10:34 +0500

    Working code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { FirstComponent } from './first/first.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponent, children: [
+    {
+      path: 'child-a', // child route path
+      component: ChildAComponent, // child route component that the router renders
+    },
+    {
+      path: 'child-b',
+      component: ChildBComponent, // another child route component that the router renders
+    },
+  ], },
+  { path: 'second-component', component: SecondComponent },
+  { path: '',   redirectTo: '/first-component', pathMatch: 'full' }, 
+  { path: '**', component: PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    PagenotfoundComponent,
+    ChildAComponent,
+    ChildBComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
